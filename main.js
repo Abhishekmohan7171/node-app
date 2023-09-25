9702cb325dd8a04694225cf1050ce058ed3b7461
@abhishek.mohan <abhishek.mohan@surboard.se>
2023-09-25 17:09:48 +0530

    v4

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const app = express();
+// const server = require("http").Server(app);
+// const port = 4000;
+// const helmet = require("helmet");
+
+// app.use(helmet());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+// //pre-flight requests
+// app.options("*", function (req, res) {
+//   res.send(200);
+// });
+
+// server.listen(port, (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   /* eslint-disable no-console */
+//   console.log("Node Endpoints working :)");
+// });
+
+// app.get("/webhooks", (err, res) => {
+//   console.log("<------------Response------------>", res);
+//   res.status(200);
+//   res.json({ working: true });
+//   res.end();
+// });
+
+// module.exports = server;
+
+// app.listen(process.env.PORT || port, ()=> console.log(`Listening to ${port}`))
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -10,7 +58,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
@@ -43,5 +90,3 @@ app.get("/webhooks", (err, res) => {
 });
 
 module.exports = server;
-
-app.listen(process.env.PORT || port, ()=> console.log(`Listening to ${port}`))
